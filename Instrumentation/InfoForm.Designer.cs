b6c8d11718bf85624d10bf460164e7a83b2a29d5
Никита Данилушкин <nekdan@ya.ru>
2021-05-30 00:56:33 +0300

    fix

--- Instrumentation/InfoForm.Designer.cs
+++ Instrumentation/InfoForm.Designer.cs
@@ -41,18 +41,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(10, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(198, 37);
+            this.label1.Size = new System.Drawing.Size(160, 30);
             this.label1.TabIndex = 1;
             this.label1.Text = "Предисловие";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Instrumentation.Properties.Resources.Безымянный;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 672);
+            this.pictureBox1.Location = new System.Drawing.Point(-37, 503);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(785, 211);
+            this.pictureBox1.Size = new System.Drawing.Size(687, 158);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -60,9 +61,9 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(18, 62);
+            this.label2.Location = new System.Drawing.Point(16, 46);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(690, 607);
+            this.label2.Size = new System.Drawing.Size(604, 455);
             this.label2.TabIndex = 3;
             this.label2.Text = resources.GetString("label2.Text");
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -70,9 +71,9 @@
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(18, 971);
+            this.label3.Location = new System.Drawing.Point(16, 728);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(690, 320);
+            this.label3.Size = new System.Drawing.Size(604, 240);
             this.label3.TabIndex = 4;
             this.label3.Text = resources.GetString("label3.Text");
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -81,26 +82,27 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(12, 915);
+            this.label4.Location = new System.Drawing.Point(10, 686);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(156, 37);
+            this.label4.Size = new System.Drawing.Size(126, 30);
             this.label4.TabIndex = 5;
             this.label4.Text = "Участники";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // InfoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(762, 722);
+            this.ClientSize = new System.Drawing.Size(667, 542);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "InfoForm";
             this.ShowIcon = false;
             this.Text = "Информация";
