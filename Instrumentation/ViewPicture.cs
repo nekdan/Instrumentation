623ad897a1bf75cf587ef08a90f4720f692adbbf
Никита Данилушкин <nekdan@ya.ru>
2021-08-19 21:55:48 +0300

    Поправил открытие картинок по клику + поправил некоторые треки

---
+++ Instrumentation/ViewPicture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Instrumentation
+{
+    public partial class ViewPicture : Form
+    {
+        public ViewPicture()
+        {
+            InitializeComponent();
+        }
+
+        public ViewPicture(Image img)
+        {
+            InitializeComponent();
+            pictureBox1.Image = img;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
