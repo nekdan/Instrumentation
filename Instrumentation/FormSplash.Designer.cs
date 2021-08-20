6f395ef5e4e1e0158705b7baf45899d393e0d08a
nekdan <nekdan@ya.ru>
2021-08-20 14:27:10 +0300

    Увеличил отображение заставки до 3 секунд

--- Instrumentation/FormSplash.Designer.cs
+++ Instrumentation/FormSplash.Designer.cs
@@ -101,7 +101,7 @@ namespace Instrumentation
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 2000;
+            this.timer1.Interval = 3000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // FormSplash
